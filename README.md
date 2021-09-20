a30d1aebf696596d91d064b6877a3b5e40debadd
Daniel Kiss <littlesnorrboy@gmail.com>
2021-09-20 14:17:15 +0200

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+## Start local
+
+### Install deps
+
+-   [rust](TBA)
+-   [wasm-pack](TBA)
+-   Run `yarn`
+
+### Run
+
+```sh
+yarn dev
+```
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import App from './App.svelte';
+import wasm from '../core/Cargo.toml';
+
+const init = async () => {
+    const core = await wasm();
+
+
+    const app = new App({
+        target: document.body,
+        props: {
+          // https://svelte.dev/docs#Creating_a_component
+          greet: core.greet()
+        }
+    });
+
+};
+
+init();
