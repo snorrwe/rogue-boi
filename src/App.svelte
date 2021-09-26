b74922fb8741ec68a09593480d9f99c9846a6894
Daniel Kiss <littlesnorrboy@gmail.com>
2021-09-26 22:23:58 +0200

    Tweak css

--- src/App.svelte
+++ src/App.svelte
@@ -35,23 +35,7 @@
 
 <style>
 	main {
-		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
-        background: black;
-	}
-
-	pre {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
