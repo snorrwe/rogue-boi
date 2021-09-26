90890e51efadc8c874918729cf4326f553727888
Daniel Kiss <littlesnorrboy@gmail.com>
2021-09-26 21:00:51 +0200

    Add an initial tick before starting the application

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import wasm from '../core/Cargo.toml';
 
 const init = async () => {
 	const core = (await wasm()).initCore();
+    core.tick(0); // initial tick
 
 	const app = new App({
 		target: document.body,
