05fe458f8959e58a236c8091d2f9ea05d6703232
Daniel Kiss <littlesnorrboy@gmail.com>
2022-09-17 18:10:16 +0200

    Init cypress

---
+++ cypress/e2e/new_game.cy.js
@@ -0,0 +1,12 @@
+describe('newgame loads', () => {
+	it('newgame button visits #newgame', () => {
+		cy.visit('/');
+		cy.clickLink('New Game');
+		cy.url().should('include', '#game');
+	});
+	it('loads', () => {
+		cy.visit('/#newgame');
+		cy.url().should('include', '#game');
+		cy.get('.game-ui').should('exist');
+	});
+});
