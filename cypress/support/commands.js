22e16c030b160fbbd2db482511560173c84c371d
Daniel Kiss <littlesnorrboy@gmail.com>
2022-09-17 18:10:16 +0200

    Init cypress

---
+++ cypress/support/commands.js
@@ -0,0 +1,7 @@
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+Cypress.Commands.add('clickLink', (label) => {
+	cy.get('a').contains(label).click();
+});
