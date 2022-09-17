76a0634267f2c2d5b8e8d27fe6f313f19cf8e7a8
Daniel Kiss <littlesnorrboy@gmail.com>
2022-09-17 21:24:34 +0200

    Assert that canvas is in the scene

--- cypress/e2e/new_game.cy.js
+++ cypress/e2e/new_game.cy.js
@@ -8,5 +8,6 @@ describe('newgame loads', () => {
 		cy.visit('/#newgame');
 		cy.url().should('include', '#game');
 		cy.get('.game-ui').should('exist');
+		cy.get('canvas').should('exist');
 	});
 });
