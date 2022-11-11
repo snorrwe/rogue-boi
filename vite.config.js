57f5a2bbcb2feeec4831ee2d725c8d9f72204e6c
Daniel Kiss <littlesnorrboy@gmail.com>
2022-11-11 20:02:40 +0100

    Fix bundles

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import { ViteRsw } from 'vite-plugin-rsw';
 import path from 'path';
 import { child_process } from 'vite-plugin-child-process';
 
-const production = process.env.BUILD === 'production';
-
 export default defineConfig({
 	appType: 'spa',
 	resolve: {
@@ -20,21 +18,12 @@ export default defineConfig({
 		ViteRsw(),
 		svelte({
 			include: ['ui/**/*svelte'],
-			compilerOptions: {
-				dev: !production
-			},
 			emitCss: false
 		}),
 		child_process({
 			name: 'icons',
 			command: ['cargo', 'xtask', 'copy-icons'],
 			watch: []
-		}),
-		production &&
-			child_process({
-				name: 'bundle',
-				command: ['cargo', 'xtask', 'bundle'],
-				watch: []
-			})
+		})
 	]
 });
