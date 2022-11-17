f1a51673930348ab583e4750de8d1a18ce1ddadc
Daniel Kiss <littlesnorrboy@gmail.com>
2022-11-17 15:11:15 +0100

    Init nix-shell

---
+++ default.nix
@@ -0,0 +1,20 @@
+let
+  rust_overlay = import (builtins.fetchTarball
+    "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  nixpkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rust_channel = nixpkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+in
+with nixpkgs;
+
+# Define the shell
+pkgs.mkShell {
+  nativeBuildInputs = [
+    nixpkgs.bzip2
+    nodePackages.yarn
+    rust_channel # Full rust from overlay, includes cargo
+    wasm-pack
+  ];
+  installPhase =''
+      ${nixpkgs.bzip2.postInstall}
+  '';
+}
