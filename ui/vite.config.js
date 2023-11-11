a854248734bd0b8bc22dd3faa324368a450a411a
Daniel Kiss <littlesnorrboy@gmail.com>
2023-11-11 12:13:27 +0100

    Fix paths when deploying

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from "node:url";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-  plugins: [svelte()],
-  resolve: {
-    alias: {
-      "@rogueBoi": fileURLToPath(new URL("./src/lib", import.meta.url))
+    base: "",
+    plugins: [svelte()],
+    resolve: {
+        alias: {
+            "@rogueBoi": fileURLToPath(new URL("./src/lib", import.meta.url))
+        }
     }
-  }
 });
