400c46378bdf458891e9bea6e7a42ecc0e2c951b
Daniel Kiss <littlesnorrboy@gmail.com>
2025-09-20 23:37:07 +0200

    fmt

--- ui/src/lib/store.js
+++ ui/src/lib/store.js
@@ -9,12 +9,12 @@ export const equipment = writable(null);
 export const selected = writable(null);
 
 export const fetchIcon = ({ name, src }) =>
-  fetch(src)
-    .then((r) => r.text())
-    .then((data) => {
-      icons.update((ic) => ({
-        [name]: data,
-        ...ic
-      }));
-      return data;
-    });
+    fetch(src)
+        .then((r) => r.text())
+        .then((data) => {
+            icons.update((ic) => ({
+                [name]: data,
+                ...ic
+            }));
+            return data;
+        });
