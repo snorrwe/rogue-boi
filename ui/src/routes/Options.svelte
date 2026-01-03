d24a13d8cf83218e6ff5c5c7a828a9eac124b6aa
Daniel Kiss <littlesnorrboy@gmail.com>
2026-01-03 17:22:00 +0100

    Allow downloading saves

--- ui/src/routes/Options.svelte
+++ ui/src/routes/Options.svelte
@@ -6,6 +6,20 @@
     coreStore.set(null);
     window.location.reload();
   }
+
+  function downloadSave() {
+    const data = localStorage.getItem("save");
+
+    let link = document.createElement("a");
+    link.href = `data:text,${data}`;
+    link.download = "rogueBoi.save";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 </script>
 
-<button class="bg-red-800 my-2" onclick={clearSave}>Clear save game</button>
+<button class="bg-red-800 my-2 p-2 rounded-2xl" onclick={clearSave}>Clear save game</button>
+<button class="bg-amber-900 my-2 p-2 rounded-2xl" onclick={downloadSave}
+  >Download the current save file</button
+>
