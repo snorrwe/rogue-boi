2ec377d09736694db7d769472cd659c1d0255131
Daniel Kiss <littlesnorrboy@gmail.com>
2026-03-14 16:02:19 +0100

    Show inventory debug on ui

--- ui/src/lib/game/Shop.svelte
+++ ui/src/lib/game/Shop.svelte
@@ -1,5 +1,13 @@
 <script>
   import { coreOutput } from "@rogueBoi/store.js";
+
+  let shop = $derived($coreOutput?.shop);
+  let inventory = $derived(shop?.inventory);
 </script>
 
-Shop
+<h1 class="text-3xl">Shop</h1>
+
+<pre>
+====inventory====
+    {JSON.stringify(inventory, null, 4)}
+</pre>
