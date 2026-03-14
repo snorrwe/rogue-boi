a39e7b54fbcb03e69042b5279d9f39ccfa9d32d6
Daniel Kiss <littlesnorrboy@gmail.com>
2026-03-14 21:15:32 +0100

    List items and costs on shop screen

--- ui/src/lib/game/Shop.svelte
+++ ui/src/lib/game/Shop.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { coreOutput } from "@rogueBoi/store.js";
+  import { coreOutput, icons } from "@rogueBoi/store.js";
 
   let shop = $derived($coreOutput?.shop);
   let inventory = $derived(shop?.inventory);
@@ -7,7 +7,22 @@
 
 <h1 class="text-3xl">Shop</h1>
 
-<pre>
-====inventory====
-    {JSON.stringify(inventory, null, 4)}
-</pre>
+<div class="mt-8 grid grid-cols-4 align-middle gap-4">
+  {#each inventory?.items ?? [] as item}
+    <div class="border-2 p-4 align-middle content-center justify-center">
+      {#if item}
+        <div class="flex flex-col">
+          <span>
+            {@html $icons[item.tag]}
+            {item.tag}
+          </span>
+          <span>
+            Cost: {item.cost}
+          </span>
+        </div>
+      {:else}
+        Empty
+      {/if}
+    </div>
+  {/each}
+</div>
