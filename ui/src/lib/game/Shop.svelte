d269c39866c835b9af7787fbd772a3378459abf1
Daniel Kiss <littlesnorrboy@gmail.com>
2026-04-04 22:42:32 +0200

    Factor out ShopItems

--- ui/src/lib/game/Shop.svelte
+++ ui/src/lib/game/Shop.svelte
@@ -1,5 +1,6 @@
 <script>
-  import { coreOutput, icons } from "@rogueBoi/store.js";
+  import { coreOutput } from "@rogueBoi/store.js";
+  import ShopItem from "./ShopItem.svelte";
 
   let shop = $derived($coreOutput?.shop);
   let inventory = $derived(shop?.inventory);
@@ -9,22 +10,6 @@
 
 <div class="mt-8 grid grid-cols-4 align-middle gap-4">
   {#each inventory ?? [] as item}
-    <div class="border-2 p-4 align-middle content-center justify-center">
-      {#if item}
-        <div class="flex flex-col">
-          <span style="--fill-color: {item.color || 'white'}">
-            {@html $icons[item.icon]}
-          </span>
-          <span>
-            {item.tag}
-          </span>
-          <span>
-            Cost: {item.cost}
-          </span>
-        </div>
-      {:else}
-        Empty
-      {/if}
-    </div>
+    <ShopItem {item} />
   {/each}
 </div>
---
+++ ui/src/lib/game/ShopItem.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { icons } from "@rogueBoi/store.js";
+
+  let { item } = $props();
+</script>
+
+<div class="border-2 p-4 align-middle content-center justify-center">
+  {#if item}
+    <div class="flex flex-col">
+      <span style="--fill-color: {item.color || 'white'}">
+        {@html $icons[item.icon]}
+      </span>
+      <span>
+        {item.tag}
+      </span>
+      <span>
+        Cost: {item.cost}
+      </span>
+    </div>
+  {:else}
+    Empty
+  {/if}
+</div>
